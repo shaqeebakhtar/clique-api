4110f39bd99d6d727a167cc9e9f4b3c9bf2fd96b
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-03-27 11:38:10 +0530

    fixing redirect routes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(router);
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
 });
 
 // sockets
